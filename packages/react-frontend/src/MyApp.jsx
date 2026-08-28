954864c142bad173d50c6f52230a05540baf0fd8
Javier Gonzalez Jr. <javiergonzalezjr.123@gmail.com>
2026-08-28 16:36:30 -0700

    finished step 6

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import Table from "./Table";
 
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+    name: "Mac",
+    job: "Bouncer",
+    },
+    {
+    name: "Dee",
+    job: "Aspring actress",
+    },
+    {
+    name: "Dennis",
+    job: "Bartender",
+    },
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
-  );
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+  </div>
+);
+
 }
+
+
+
 export default MyApp;
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 
-function Table() {
+function TableHeader() {
+  return <tbody />;
+}
+
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
+    );
+   }
+  );
+  return (
+      <tbody>
+        {rows}
+       </tbody>
+   );
+}
+
+function Table(props) {
   return (
     <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
+      <TableHeader />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
     </table>
   );
 }
