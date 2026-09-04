74663ac99da9799232dc8c3b47a468550cee61e5
Javier Gonzalez Jr. <javiergonzalezjr.123@gmail.com>
2026-09-04 15:14:58 -0700

    step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,19 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
